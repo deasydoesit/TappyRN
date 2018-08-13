077dbf6c300395e635d1a831e55660d37bdce359
deasydoesit <darwin.rinderer@gmail.com>
2018-08-13 12:27:04 -0400

    Restructuring files

--- App.js
+++ App.js
@@ -1,39 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Platform,
+  Button, 
+  View
+} from "react-native";
+import { BleManager, Device, BleError, LogLevel } from "react-native-ble-plx";
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor() {
+    super()
+    this.manager = new BleManager()
+    this.state = {info: "", values: {}}
+  }
+
+  info(message) {
+    this.setState({info: message})
+  }
+
+  error(message) {
+    this.setState({info: "ERROR: " + message})
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'ios') {
+      this.manager.onStateChange((state) => {
+        if (state === 'PoweredOn') this.scanAndConnect()
+      })
+    } else {
+      this.scanAndConnect()
+    }
+  }
+
+  scanAndConnect() {
+    this.manager.startDeviceScan(null, null, (error, device) => {
+      this.info("Scanning...")
+      console.log(device)
+
+      if (error) {
+        this.error(error.message)
+        return
+      }
+
+      if (device.name === 'tappy' || device.name === 'Tappy') {
+        this.info("Connecting to TI Sensor")
+        this.manager.stopDeviceScan()
+        device.connect()
+          .then((device) => {
+            this.info("Discovering services and characteristics")
+            return device.discoverAllServicesAndCharacteristics()
+          })
+          .then((device) => {
+            this.info("Setting notifications")
+            return this.setupNotifications(device)
+          })
+          .then(() => {
+            this.info("Listening...")
+          }, (error) => {
+            this.error(error.message)
+          })
+      }
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello Tappy!</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <FlatList
+          style={styles.container}
+          data={this.state.text}
+          renderItem={({ item }) => <Text> {item} </Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   },
   welcome: {
     fontSize: 20,
---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  TappyRN
+//
+//  Created by Przemysław Lenart on 20/07/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
